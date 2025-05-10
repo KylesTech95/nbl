824be0aede37a38f715293b0d90bbb265b68f0ac
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2025-05-10 07:58:24 -0400

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 5535;
+const cors = require('cors')
+const bp = require('body-parser')
+
+// middleware
+app.use(cors())
+app.use(bp.urlencoded())
+app.use(express.json())
+
+
+// route
+app.route('/').get((req,res)=>{
+    res.send('Hello NBL');
+})
+
+
+
+
+app.listen((port),()=>{
+    console.log('listening on port: ' + port)
+})
