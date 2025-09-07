f72b715a53cd88ea6c14348a84b5705abdd2e444
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2025-09-07 07:51:11 -0400

    chore: update auth strategy with URI (failure)

--- server/lib/passport/strategy.js
+++ server/lib/passport/strategy.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const passport = require('passport')
 /* ---------------------------- Local ---------------------------- */
 var LocalStrategy = require('passport-local').Strategy;
@@ -18,12 +19,12 @@ var LocalStrategy = require('passport-local').Strategy;
 
 
 /* ---------------------------- Google ---------------------------- */
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+// var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // passport.use(new GoogleStrategy({
-//     clientID: GOOGLE_CLIENT_ID,
-//     clientSecret: GOOGLE_CLIENT_SECRET,
-//     callbackURL: "http://www.example.com/auth/google/callback"
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_SECRET,
+//     callbackURL: "http://localhost/auth/google/callback"
 //   },
 //   function(accessToken, refreshToken, profile, cb) {
 //     User.findOrCreate({ googleId: profile.id }, function (err, user) {
